6de95e8690e767f05aa96c3f541fa83d992d40c1
Dawson Dohlen <d.dohlen@pm.me>
2024-08-27 19:17:10 -0700

    first draft blog page html and some javascript outlines

---
+++ assets/js/blog.js
@@ -0,0 +1,9 @@
+// get the blog articles out of local storage 
+
+// display them 
+
+const buttonEl = document.querySelector("#submit");
+
+buttonEl.addEventListener("click", function () {
+  window.location = "./index.html";
+});
